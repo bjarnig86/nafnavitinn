9b84de4c973fa081ef92b25aee027c1d2bed15d4
bjarnig86 <72604468+bjarnig86@users.noreply.github.com>
2023-01-15 16:40:29 +0000

    initial commit

---
+++ ble.py
@@ -0,0 +1,17 @@
+﻿import requests
+from bs4 import BeautifulSoup
+
+r = requests.get("https://attavitinn.is/samfelagid/merkingar-nafna/")
+
+soup = BeautifulSoup(r.content, features='html.parser') 
+
+rows = soup.find_all('div', id="name-wraper")
+
+print(len(rows))
+
+for row in rows:
+    h3 = row.find_all('h3')
+    p = row.find_all('p')
+    for i in range(len(h3)):
+        print(h3[i].text.strip())
+        print(p[i].text.strip())
